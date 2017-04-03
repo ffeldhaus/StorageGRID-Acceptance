d619b11691e03f79a18533c629037f60a1a474c8
Florian Feldhaus <florian.feldhaus@gmail.com>
2017-04-03 15:47:35 +0200

    coordinated download time

--- performance-acceptance-tests.sh
+++ performance-acceptance-tests.sh
@@ -239,11 +239,11 @@ TIMEFORMAT=%0R
         echo "$(date '+%Y-%m-%d %H:%M:%S') Starting download"
         ( set -x;dd if=$FILENAME of=/dev/null )
         echo "$(date '+%Y-%m-%d %H:%M:%S') $COUNT files downloaded"
-      )
       NEXT_START_HOUR=$(( ( $NEXT_START + ($SIZE / 2 / $WORKER_COUNT) ) >= 60 ? ( $(date "+%H") + 1 ) : $(date "+%H") ))
       NEXT_START=$(( ( $NEXT_START + ($SIZE / 2 / $WORKER_COUNT) ) >= 60 ? ( $NEXT_START - (60 - ($SIZE / 2 / $WORKER_COUNT) ) ) : ( $NEXT_START + ($SIZE / 2 / $WORKER_COUNT) ) ))
       NEXT_START=$(printf "%02d" $NEXT_START)
       echo "Next download will start at $NEXT_START_HOUR:$NEXT_START"
+      )
     done 2>&1
     echo "FINISHED"
   )
