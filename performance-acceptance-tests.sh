6412a823e323166068e16add63c44eacd7fdf351
Florian Feldhaus <florian.feldhaus@gmail.com>
2017-04-03 15:02:28 +0200

    coordinated download time

--- performance-acceptance-tests.sh
+++ performance-acceptance-tests.sh
@@ -220,14 +220,14 @@ TIMEFORMAT=%0R
         unset FILENAME
         while [ -z $FILENAME ];do
           if [ $COUNT -le $UPLOAD_COUNT ];then
-            if [[ "$((10#$(date "+%M") ))" -eq "$((10#$NEXT_START))" ]]; then
+            if [[ "$(( 10#$(date "+%M") ))" -eq "$(( 10#$NEXT_START ))" ]]; then
               FILENAME=$(find $DOWNLOAD_SOURCE/${PREFIX}${SIZE}g${COUNT} -not -size -${SIZE}G 2> /dev/null)
             elif [[ "$((10#$(date "+%M") ))" -ge "$((10#$NEXT_START))" ]]; then
               echo "File $DOWNLOAD_SOURCE/${PREFIX}${SIZE}g${COUNT} not yet ready"
               NEXT_START_HOUR=$(( ( $NEXT_START + ($SIZE / 2 / $WORKER_COUNT) ) >= 60 ? ( $(date "+%H") + 1 ) : $(date "+%H") ))
               NEXT_START=$(( ( $NEXT_START + ($SIZE / 2 / $WORKER_COUNT) ) >= 60 ? ( $NEXT_START - (60 - ($SIZE / 2 / $WORKER_COUNT) ) ) : ( $NEXT_START + ($SIZE / 2 / $WORKER_COUNT) ) ))
               NEXT_START=$(printf "%02d" $NEXT_START)
-              SLEEP_TIME=$(( ( $NEXT_START - $(date "+%M") ) * 60 - $(date "+%S") ))
+              SLEEP_TIME=$(( ( 10#$NEXT_START - 10#$(date "+%M") ) * 60 - 10#$(date "+%S") ))
               echo "Next download attempt will start at $NEXT_START_HOUR:$NEXT_START"
               sleep $SLEEP_TIME
             fi
